ab424f2ca80e8194902a214305abfb3272c41543
hmluck83 <hmluck83@gmail.com>
2024-04-12 10:44:43 +0900

    \n2

--- python/hmluck83.py
+++ python/hmluck83.py
@@ -2,7 +2,7 @@ import pandas as pd
 from common import OUTPUT_PATH, MEASUREMENTS_PATH, Timer
 
 def solution(path: str) -> str:
-    return '\n'.join([f"{i[1].name}={int(i[1]['value']['min'])};{int(i[1]['value']['max'])};{int(i[1]['value']['mean'])}({int(i[1]['value']['sum'])}/{int(i[1]['value']['count'])})" for i in pd.read_table("measurements.txt", header=None, delimiter=";", names=['city', 'value']).groupby(['city']).agg({'value':['mean', 'max', 'min', 'count', 'sum']}).iterrows()])+"\n"
+    return '\n'.join([f"{i[1].name}={int(i[1]['value']['min'])};{int(i[1]['value']['max'])};{int(i[1]['value']['mean'])}({int(i[1]['value']['sum'])}/{int(i[1]['value']['count'])})" for i in pd.read_table(path, header=None, delimiter=";", names=['city', 'value']).groupby(['city']).agg({'value':['mean', 'max', 'min', 'count', 'sum']}).iterrows()])+"\n"
 
 def main():
     with open(OUTPUT_PATH, 'r') as f:
