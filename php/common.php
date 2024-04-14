0ea10291d1af626003c9546ce8a9db3c698119d8
kit rio <mario64aq@gmail.com>
2024-04-14 21:43:13 +0900

    오타 수정

--- php/common.php
+++ php/common.php
@@ -1,3 +1,3 @@
 <?php
 const OUT_PUT_PATH = __DIR__ . DIRECTORY_SEPARATOR . 'outputs.txt';
-const MEASUREMENTS_PATH = __DIR__ . DIRECTORY_SEPARATOR ."measurements.txt"
+const MEASUREMENTS_PATH = __DIR__ . DIRECTORY_SEPARATOR ."measurements.txt";
