cec593cd33610de00f280deb387b35295d528e3c
lemon-mint <ice.lemons.mint@gmail.com>
2024-04-15 10:55:50 +0900

    lemon-mint3

--- run_go.sh
+++ run_go.sh
@@ -4,3 +4,9 @@ sh go/run_basic.sh
 
 echo "[GO] lemon-mint1 example"
 sh go/run_lemon-mint1.sh
+
+echo "[GO] lemon-mint2 example"
+sh go/run_lemon-mint1.sh
+
+echo "[GO] lemon-mint3 example"
+sh go/run_lemon-mint1.sh
