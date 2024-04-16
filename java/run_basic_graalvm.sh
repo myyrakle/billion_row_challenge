340321e65d7baec0d1f5a21d0f8fe3c3932313e0
lemon-mint <ice.lemons.mint@gmail.com>
2024-04-16 01:44:39 +0000

    Update run_basic_graalvm.sh

--- java/run_basic_graalvm.sh
+++ java/run_basic_graalvm.sh
@@ -1,5 +1,6 @@
 cd java/basic && \
   javac Main.java && \
-  native-image -O3 Main && \
-  ./main && \
-  cd .. && cd ..
+  native-image -O3 -march=native --gc=G1 Main && \
+  mv ./main ../../main && \
+  cd .. && cd .. && \
+  ./main
