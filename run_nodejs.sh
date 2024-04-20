ff24d07191cdae943a7a01e1aecd64f783c5a931
Ian <pravda.kracota@gmail.com>
2024-04-21 06:51:09 +0900

    Update run_nodejs.sh
    resolve conflict

--- run_nodejs.sh
+++ run_nodejs.sh
@@ -2,16 +2,11 @@
 echo "[Node.js] BASIC example"
 sh nodejs/run_basic.sh
 
-<<<<<<< HEAD
 echo "[Node.js] subrabbit1"
 sh nodejs/run_sunrabbit1.sh
 
 echo "[Node.js] subrabbit2"
 sh nodejs/run_sunrabbit2.sh
-=======
-echo "[Node.js] subrabbit1 example"
-sh nodejs/run_sunrabbit1.sh
 
-echo "[Node.js] prravda example"
+echo "[Node.js] prravda"
 sh nodejs/run_prravda.sh
->>>>>>> prravda
